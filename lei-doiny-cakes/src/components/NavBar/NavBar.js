309e9f92879e38771d056774495355bdea12064b
tatiana-soifer <tatusoifer@gmail.com>
2021-05-30 23:52:36 -0300

    desafio 2

--- lei-doiny-cakes/src/components/NavBar/NavBar.js
+++ lei-doiny-cakes/src/components/NavBar/NavBar.js
@@ -1,38 +1,40 @@
-import './navBar.scss'
+import logo from '../../assets/logo/logo.png';
+import carrito from '../../assets/icons/carrito.png';
+import './NavBar.css';
 
 
-function navBar () {
-    return (
-        //LOGO
-        <img src="../img/logo.png" alt="logo" />
-        //NAVBAR
-        <nav>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <ul className="navbar-nav mt-2 mt-lg-0 ml-auto">
-                    <li className="nav-item">
-                        <a className="navbar-brand" href="#menu">Menú</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="navbar-brand" href="#como_comprar">Cómo comprar</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="navbar-brand" href="#contacto">Contacto</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="navbar-brand" href="#FAQ">FAQ</a>
-                    </li>
-                    <li className="nav-item">
-                        <button id="carrito_menu" type="button" className="btn" data-toggle="modal" data-target="#exampleModal">
-                            <img src="../../../" alt="carrito de compras" />
-                        </button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+function NavBar(){
+    return(
+        <div>
+            <img src={logo} alt="logo" id="logo" />
+            <nav>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <ul className="navbar-nav mt-2 mt-lg-0 ml-auto">
+                        <li className="nav-item">
+                            <a className="navbar-brand" href="#menu">Menú</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="navbar-brand" href="#como_comprar">Cómo comprar</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="navbar-brand" href="#contacto">Contacto</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="navbar-brand" href="#FAQ">FAQ</a>
+                        </li>
+                        <li className="nav-item">
+                            <button id="carrito_menu" type="button" className="btn" data-toggle="modal" data-target="#exampleModal">
+                                <img src={carrito} alt="carrito de compras" />
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     );
 }
 
-export default navBar;
+export default NavBar;
