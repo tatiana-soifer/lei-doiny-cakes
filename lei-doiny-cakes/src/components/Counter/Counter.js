e9a5bdd98a057cfd678c4b8930774a782f1a4ab3
tatiana-soifer <tatusoifer@gmail.com>
2021-06-04 22:01:16 -0300

    modificacion error counter.js

--- lei-doiny-cakes/src/components/Counter/Counter.js
+++ lei-doiny-cakes/src/components/Counter/Counter.js
@@ -2,26 +2,12 @@ import './Counter.css'
 import '../Main/Main.css'
 
 const Counter = ({initial, stock, onAdd}) => {
-    const [items, setItems] = useState (initial);
-    const addItems = () => {
-        if (items < stock) {
-            setItems(items + 1)
-        } else{
-            alert ('Lamentablemente no hay mas stock')
-        }
-    };
-    const lessItems = () =>{
-        setItems(items - 1)
-    }
+    
     return (
-        <div className="container">
-            <div className="row">
-                <div>
-                    <button id="boton_counter" className="btn btn-secondary boton" data-toggle="modal" onClick={addItems}>+</button>
-                    <span>{items}</span>
-                    <button id="boton_counter" className="btn btn-secondary boton" data-toggle="modal" onClick={lessItems}>-</button>
-                </div>
-            </div>
+        <div>
+            <button id="boton_counter" className="btn btn-secondary boton" data-toggle="modal">+</button>
+                <span></span>
+            <button id="boton_counter" className="btn btn-secondary boton" data-toggle="modal">-</button>
         </div>
     );
 }
