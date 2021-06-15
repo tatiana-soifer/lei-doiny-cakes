4db42aba4ab20ba5063699ebf768fdd8f29e0aaf
tatiana-soifer <tatusoifer@gmail.com>
2021-06-15 01:32:46 -0300

    modificacion del boton del navbar

--- lei-doiny-cakes/src/components/NavBar/NavBar.js
+++ lei-doiny-cakes/src/components/NavBar/NavBar.js
@@ -8,7 +8,7 @@ function NavBar(){
         <div>
             <nav id="menu" className="navbar navbar-expand-lg fixed-top">
                 <img src={logo} alt="logo" id="logo" />
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <button id="boton_navbar" className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
