e590bf34864980ce598eeb9b7b8187288f60300a
tatiana-soifer <tatusoifer@gmail.com>
2021-06-17 22:47:56 -0300

    desafios

--- lei-doiny-cakes/src/components/Item/ItemDetail/ItemDetailContainer.js
+++ lei-doiny-cakes/src/components/Item/ItemDetail/ItemDetailContainer.js
@@ -14,56 +14,64 @@ function ItemDetailContainer ({itemsDetailContainer}) {
                             Torta: 'Praline',
                             Descripcion: 'Crema pastelera de chocolate y praline de frutos secos. Tamaño: Molde 26cm',
                             Stock: 15,
-                            Precio: 1750
+                            Precio: 1750,
+                            Foto: 'https://drive.google.com/drive/folders/1zYa3Ln-CfiBxN3stbNoop5UKzzMXzVoD'
                         },
                         {
                             Id: 2,                
                             Torta: 'Franui',
                             Descripcion: 'Base crocante de chocolate, con una mousse de chocolate negro y mousse de frambuesa, bañada en chocolate blanco. Tamaño: Molde 26cm',
                             Stock: 3,
-                            Precio: 1950
+                            Precio: 1950,
+                            Foto: 'https://drive.google.com/drive/folders/1zYa3Ln-CfiBxN3stbNoop5UKzzMXzVoD'
                         },
                         {
                             Id: 3,
                             Torta: 'Choco-oreo',
                             Descripcion: 'Es la mezcla perfecta entre la Chocotorta y la Doble Oreo. Una bomba explosiva que hay que probar. Tamaño: Molde 26cm',
                             Stock: 10,
-                            Precio: 1850
+                            Precio: 1850,
+                            Foto: 'https://drive.google.com/drive/folders/1zYa3Ln-CfiBxN3stbNoop5UKzzMXzVoD'
                         },
                         {
                             Id: 4,
                             Torta: 'Doble-oreo',
                             Descripcion: 'Dos capas de Oreo, dulce de leche y crema chantillí. Tamaño: Molde 26cm',
                             Stock: 5,
-                            Precio: 1850
+                            Precio: 1850,
+                            Foto: 'https://drive.google.com/drive/folders/1zYa3Ln-CfiBxN3stbNoop5UKzzMXzVoD'
                         },
                         {
                             Id: 5,
                             Torta: 'Mousse de Chocolate',
                             Descripcion: 'Clásica mousse de chocolate negro, con una base de brownie humedo y chocolate amargo rallado por arriba. Tamaño: Molde 26cm',
                             Stock: 5,
-                            Precio: 1800
+                            Precio: 1800,
+                            Foto: 'https://drive.google.com/drive/folders/1zYa3Ln-CfiBxN3stbNoop5UKzzMXzVoD'
                         },
                         {
                             Id: 6,
                             Torta: 'Marquisse',
                             Descripcion: 'Brownie húmedo, dulce de leche, crema chantillí o merengue Italiaano y lluvia de chocolate. Tamaño: Molde 26cm',
                             Stock: 5,
-                            Precio: 1850
+                            Precio: 1850,
+                            Foto: 'https://drive.google.com/drive/folders/1zYa3Ln-CfiBxN3stbNoop5UKzzMXzVoD'
                         },
                         {
                             Id: 7,
                             Torta: 'Cheesecake',
                             Descripcion: 'Cheescake con base crocante, crema de limon y frutos rojos. Tamaño: Molde 26cm',
                             Stock: 5,
-                            Precio: 1800
+                            Precio: 1800,
+                            Foto: 'https://drive.google.com/drive/folders/1zYa3Ln-CfiBxN3stbNoop5UKzzMXzVoD'
                         },
                         {
                             Id: 8,
                             Torta: 'Crumble de Manzana',
                             Descripcion: 'Base de masa quebrada dulce, manzana y un crumble crocante. Tamaño: Molde 26cm',
                             Stock: 5,
-                            Precio: 1850
+                            Precio: 1850,
+                            Foto: 'https://drive.google.com/drive/folders/1zYa3Ln-CfiBxN3stbNoop5UKzzMXzVoD'
                         }
                     )
                 }, 2000)
