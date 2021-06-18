afaad0c6ad927d58c4ce1876a5edbef56d7e2560
tatiana-soifer <tatusoifer@gmail.com>
2021-06-18 19:40:55 -0300

    varios

--- lei-doiny-cakes/src/components/Counter/Counter.js
+++ lei-doiny-cakes/src/components/Counter/Counter.js
@@ -2,25 +2,25 @@ import React, { useState } from 'react';
 import './../css/Main.css';
 
 const Counter = ({initialState, stock, onAdd}) => {
-    const [items, setItems] = useState(initialState)
+    const [count, setItems] = useState(initialState)
     const addItems = () => {
-        if (items < stock){
-            setItems (items + 1)
+        if (count < stock){
+            setItems (count + 1)
         }
         else {
             alert ('Lamentablemente no hay mas stock')
         };
     };
     const removeItems = () =>{
-        if (items > 0 ) {
-            setItems (items - 1)
+        if (count > 0 ) {
+            setItems (count - 1)
         };
     };
     return (
         <div>
             <div className="boton_counter">
                 <button className="btn btn-secondary boton" onClick={removeItems}>-</button>
-                    <label>{items}</label>
+                    <label>{count}</label>
                 <button className="btn btn-secondary boton" onClick={addItems}>+</button>
             </div>
             <button id="boton_anadir" className="btn btn-secondary">Añadir</button>
