2c7a9aa1162b19a29d739ae7d5482485a2a53888
tatiana-soifer <tatusoifer@gmail.com>
2021-06-22 20:57:40 -0300

    varios

--- lei-doiny-cakes/src/components/Item/ItemList/ItemList.js
+++ lei-doiny-cakes/src/components/Item/ItemList/ItemList.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import Item from '../Item';
+import {Link} from 'react-router-dom';
 import '../../css/Main.css';
 
-const ItemList = ({items}) => {
-    return (
-        <div className="container">
-            <div className="row">
-                {items.map((item, i) => {
-                    return(
-                        <Item item={item} key={i} />
-                    )
-                })};
+const ItemList = ({id, Foto, Torta, Precio, Descripcion}) => {
+    return(
+        <div className="itemCard" id={id}>               
+            <div className="img-item-container">
+                <img src={Foto} alt={Descripcion}></img>
             </div>
-        </div>
+            <div>
+                <div>
+                    <h3>{Torta}</h3>
+                    <h4>{Descripcion}</h4>
+                    <p>-</p>
+                    <p>${Precio}</p>
+                    <p>-</p>
+                    <Link to={`/item/:${id}`}>Ver mas</Link> 
+                </div>
+            </div>           
+        </div>        
     );
-};
+}
 
 export default ItemList;
