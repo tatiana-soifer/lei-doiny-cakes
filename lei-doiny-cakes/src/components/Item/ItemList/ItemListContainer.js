3a8eb231e8f23056ee83e1823eab2ea66da96bd0
tatiana-soifer <tatusoifer@gmail.com>
2021-07-11 22:45:51 -0300

    agregue el loader

--- lei-doiny-cakes/src/components/Item/ItemList/ItemListContainer.js
+++ lei-doiny-cakes/src/components/Item/ItemList/ItemListContainer.js
@@ -1,32 +1,61 @@
 import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router';
+import {useParams} from 'react-router-dom';
 import {getFirestore} from '../../../Factory/Firebase.js';
 import ItemList from '../../Item/ItemList/ItemList';
+import LoaderingLoader from '../../Loader/Loader.js';
 import './../../css/Main.css';
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
     const {categoryId} = useParams();
     useEffect(() => {
+        setLoading(true);
         const db = getFirestore();
-        const itemCollection = db.collection('itemCollection');
-        if(!categoryId){            
-            itemCollection.get().then(querySnapshot => {
-            setItems(querySnapshot.docs.map(doc => (
-                {id: doc.id, ...doc.data()}
-            )));
-        });
-        } else{
-            const category = itemCollection.where('categoryId', '===', categoryId);
-            category.get().then(querySnapshot => {
-            setItems(querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()})))});
-        }
-    }, [categoryId]);
+        const itemCollection = db.collection('itemCollection');            
+            if(!categoryId){itemCollection
+                    .get()
+                    .then((querySnapshot) => {
+                    if (querySnapshot.size === 0) {
+                        console.log('No results!');
+                    }
+                    setItems(querySnapshot.docs.map((doc) => doc.data()));
+                    })
+                    .catch((e) => {
+                    console.e('Error buscando items', e);
+                    })
+                    .finally(() => {
+                    setLoading(false);
+                    });
+                }else{
+                const itemCollectionCategory = itemCollection.where('idCategory', '===', categoryId)
+                itemCollectionCategory
+                    .get()
+                    .then((querySnapshot) => {
+                    if (querySnapshot.size === 0) {
+                        console.log('No hay esultados!');
+                    }
+                    setItems(querySnapshot.docs.map((doc) => doc.data()));
+                    })
+                    .catch((error) => {
+                    console.error('Error searching items', error);
+                    })
+                    .finally(() => {
+                    setLoading(false);
+                    });
+                }
+            }, [categoryId]);
+        
     return (
         <>
+        {
+            loading ?
+            <LoaderingLoader />
+            :
             <div className="cuadriculaProductos"> 
                 <ItemList items={items} />
             </div>
+        }
         </>
     )
 };
