a85feb4ef273765ad16aad42e37175e945f3bf6e
tatiana-soifer <tatusoifer@gmail.com>
2021-07-19 23:29:38 -0300

    varios

--- lei-doiny-cakes/src/components/Cart/Cart.js
+++ lei-doiny-cakes/src/components/Cart/Cart.js
@@ -47,70 +47,78 @@ const Cart = () => {
     return (
         <div className="container-fluid">
             <div className="row">
-                {!loader ? (
-                        cart.length > 0 ? (
-                            <div className="container">
-                                <div className="row">
-                                    <div className="cart">
-                                        <div className="cartHeader">
-                                            <h2>Este es tu Carrito de Compras</h2>
-                                        </div>
-                                        <div className="cartBody">
-                                            <table className="table table-striped">
-                                                <thead>
-                                                    <tr>
-                                                        <th scope="col">Producto</th>
-                                                        <th scope="col">Precio</th>
-                                                        <th scope="col">Cantidad</th>
-                                                        <th scope="col">Subtotal</th>
-                                                        <th></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody className="tbody">
-                                                    {cart.map((producto) => {
-                                                        return(
-                                                            <tr>
-                                                                <td>
-                                                                    <h4>{producto.item.title}</h4>
-                                                                </td>
-                                                                <td className="tablaPrecio">{producto.item.price}</td>
-                                                                <td className="tablaCantidad">{producto.item.quantity}</td>
-                                                                <td className="tablaSubtotal">{producto.item.price * producto.quantity}</td>
-                                                                <td>
-                                                                    <button className="delete btn btn-danger botonCarritoBorrar" onClick={() => removeItem(producto.item.id)}>Cancelar</button>
-                                                                </td>
-                                                            </tr>
-                                                        );
-                                                    })};
-                                                </tbody>
-                                            </table>
-                                            <div className="row">
-                                                <button className="btn" onClick={() => clear()}>Vaciar Carrito</button>
+                {
+                    !loader ? (
+                            cart.length > 0 ? (
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="cart">
+                                            <div className="cartHeader">
+                                                <h2>Este es tu Carrito de Compras</h2>
                                             </div>
-                                            <div className="col">
-                                                <h3 className="itemTotalCarrito">Total a Pagar:{total}</h3>
+                                            <div className="cartBody">
+                                                <table className="table table-striped">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">Producto</th>
+                                                            <th scope="col">Precio</th>
+                                                            <th scope="col">Cantidad</th>
+                                                            <th scope="col">Subtotal</th>
+                                                            <th></th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody className="tbody">
+                                                        {cart.map((producto) => {
+                                                            return(
+                                                                <tr>
+                                                                    <td>
+                                                                        <h4>{producto.item.title}</h4>
+                                                                    </td>
+                                                                    <td className="tablaPrecio">{producto.item.price}</td>
+                                                                    <td className="tablaCantidad">{producto.item.quantity}</td>
+                                                                    <td className="tablaSubtotal">{producto.item.price * producto.quantity}</td>
+                                                                    <td>
+                                                                        <button className="delete btn btn-danger botonCarritoBorrar" onClick={() => removeItem(producto.item.id)}>Cancelar</button>
+                                                                    </td>
+                                                                </tr>
+                                                            );
+                                                        })};
+                                                    </tbody>
+                                                </table>
+                                                <div className="row">
+                                                    <button className="btn" onClick={() => clear()}>Vaciar Carrito</button>
+                                                </div>
+                                                <div className="col">
+                                                    <h3 className="itemTotalCarrito">Total a Pagar:{total}</h3>
+                                                </div>
+                                                <Formulario finalizarCompra={finalizarCompra} formData={formularioData} setFormData={setFormularioData} />
                                             </div>
-                                            <Formulario finalizarCompra={finalizarCompra} formData={formularioData} setFormData={setFormularioData} />
                                         </div>
                                     </div>
                                 </div>
+                            ) : (
+                                <div>
+                                    {order.generated ? (
+                                        <>
+                                            <h2>Muchas gracias por tu compra!</h2>
+                                            <p>El ID de tu Pedido es: {order.id}</p>
+                                        </>
+                                    ) : (
+                                        <p>El carrito está vacío.</p>
+                                    )}
+                                    <Link className="btn" exact to={"/"}>Volver al Menu Principal</Link>
+                                </div>
+                            )
+                    ) : (
+                        <div className="container">
+                            <div className="row">
+                                <div className="loader">
+                                    <Loader />
+                                </div>
                             </div>
-                        ) : (
-                            <div>
-                                {order.generated ? (
-                                    <>
-                                        <h2>Muchas gracias por tu compra!</h2>
-                                        <p>El ID de tu Pedido es: {order.id}</p>
-                                    </>
-                                ) : (
-                                    <p>El carrito está vacío.</p>
-                                )}
-                                <Link className="btn" exact to={"/"}>Volver al Menu Principal</Link>
-                            </div>
-                        )
-                ) : (
-                    <Loader />
-                )}
+                        </div>
+                    )
+                }
             </div>
         </div>
     )
