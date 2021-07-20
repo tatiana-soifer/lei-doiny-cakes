ac6355a0639b07273440b2e3d9fd825be0277e3a
tatiana-soifer <tatusoifer@gmail.com>
2021-07-19 21:39:29 -0300

    varios

--- lei-doiny-cakes/src/components/Cart/Cart.js
+++ lei-doiny-cakes/src/components/Cart/Cart.js
@@ -1,16 +1,17 @@
-import React, {useContext, useState} from "react";
-import {Link} from "react-router-dom";
+import React, {useContext, useState} from 'react';
+import {Link} from 'react-router-dom';
+import {getFirestore} from '../../Factory/Firebase.js';
 import CartContext from '../Context/CartContext.js';
 import Loader from '../../Loader/Loader.js';
-import firebase from "firebase/app";
-import {getFirestore} from "../../Factory/Firebase.js";
+import firebase from 'firebase/app';
+import Formulario from '../Cart/Formulario.js'
 import '../css/Main.css';
 
 const Cart = () => {
     const {cart, clear, removeItem} = useContext (CartContext);
     const [loader, setLoader] = useState(false);
     const [order, setOrder] = useState({ id: "", generated: false });
-    const [formData, setFormData] = useState({});
+    const [formularioData, setFormularioData] = useState({});
     let total = cart.reduce (function (previo, actual) {
         return previo + actual.item.price * actual.quantity;
     }, 0);
@@ -19,11 +20,10 @@ const Cart = () => {
         const db = getFirestore();
         const orders = db.collection("orders");
         const buyer = {
-            email: formData.email || '',
-            name: formData.name || '',
-            phone: formData.phone || '',
+            email: formularioData.email || '',
+            name: formularioData.name || '',
+            phone: formularioData.phone || '',
         };
-    
         const newOrder = {
             buyer: buyer,
             items: cart,
@@ -45,66 +45,82 @@ const Cart = () => {
             });
         };
     return (
-        <>
-            {cart.length === 0 && 
-                <div>
-                    <h3>No hay productos en tu carrito.</h3>
-                    <Link className="btn btn-primary" to={"/"}>Ir al Menu</Link>
-                </div>
-            }
-            {cart.length !== 0 && (
-                <div className="container">
-                    <div className="row">
-                        <div className="cart">
-                            <div className="cartHeader">
-                                <h2>Carrito de Compras</h2>
-                            </div>
-                            <div className="cartBody">
-                                <table className="table table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Producto</th>
-                                            <th scope="col">Precio</th>
-                                            <th scope="col">Cantidad</th>
-                                            <th scope="col">Subtotal</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="tbody">
-                                        {cart.map((producto) => {
-                                            return(
-                                                <tr>
-                                                    <td>
-                                                        <img className="tablaImg" src={producto.item.photo} alt={producto.item.description}/>
-                                                        <h4 className="tablaTitulo">{producto.item.photo}</h4>
-                                                    </td>
-                                                    <td className="tablaPrecio">{producto.item.price}</td>
-                                                    <td className="tablaCantidad">{producto.item.quantity}</td>
-                                                    <td className="tablaSubtotal">{producto.item.price * producto.quantity}</td>
-                                                    <td>
-                                                        <button className="delete btn btn-danger botonCarritoBorrar" onClick={() => removeItem(producto.item.id)}>Cancelar</button>
-                                                    </td>
-                                                </tr>
-                                            );
-                                        })};
-                                    </tbody>
-                                </table>
-                                <div className="row">
-                                    <button className="btn btn-info" onClick={() => clear()}>Vaciar carrito</button>
-                                </div>
-                                <div className="col">
-                                    <h3 className="itemTotalCarrito">Total a pagar:{total}</h3>
-                                </div>
-                                <div className="row mx-4">
-                                    <div className="col d-flex justify-content-end">
-                                        <Link id="botonCartFinal" className="btn btn-secondary" exact to={'/'}>Seguir comprando</Link>
-                                        <button className="btn btn-secondary" type="button">Finalizar compra</button>
+        <div className="container">
+            <div className="row">
+                    {cart.length === 0 && 
+                        <div>
+                            <h3>No hay productos en tu carrito.</h3>
+                            <Link className="btn btn-primary" to={"/"}>Ir al Menu</Link>
+                        </div>
+                    }
+                    {cart.length !== 0 && (
+                        <div className="container">
+                            <div className="row">
+                                {!loading ? (
+                                    cart.length > 0 ? (
+                                        <div className="cart">
+                                            <div className="cartHeader">
+                                                <h2>Carrito de Compras</h2>
+                                            </div>
+                                            <div className="cartBody">
+                                                <table className="table table-striped">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">Producto</th>
+                                                            <th scope="col">Precio</th>
+                                                            <th scope="col">Cantidad</th>
+                                                            <th scope="col">Subtotal</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody className="tbody">
+                                                        {cart.map((producto) => {
+                                                            return(
+                                                                <tr>
+                                                                    <td>
+                                                                        <img className="tablaImg" src={producto.item.photo} alt={producto.item.description}/>
+                                                                        <h4 className="tablaTitulo">{producto.item.photo}</h4>
+                                                                    </td>
+                                                                    <td className="tablaPrecio">{producto.item.price}</td>
+                                                                    <td className="tablaCantidad">{producto.item.quantity}</td>
+                                                                    <td className="tablaSubtotal">{producto.item.price * producto.quantity}</td>
+                                                                    <td>
+                                                                        <button className="delete btn btn-danger botonCarritoBorrar" onClick={() => removeItem(producto.item.id)}>Cancelar</button>
+                                                                    </td>
+                                                                </tr>
+                                                            );
+                                                        })};
+                                                    </tbody>
+                                                </table>
+                                                <div className="row">
+                                                    <button className="btn btn-info" onClick={() => clear()}>Vaciar carrito</button>
+                                                </div>
+                                                <div className="col">
+                                                    <h3 className="itemTotalCarrito">Total a pagar:{total}</h3>
+                                                </div>
+                                                <Formulario finalizarCompra={finalizarCompra} formData={formularioData} setFormData={setFormularioData}/>
+                                                ) : (
+                                            <div>
+                                                {order.generated ? (
+                                                <>
+                                                    <h2>Compra finalizada con exito.</h2>
+                                                    <p>Orden ID: {order.id}</p>
+                                                </>
+                                                ) : (
+                                                <p>El carrito está vacío.</p>
+                                                )}
+                                                <NavLink className="btn btn-primary" exact to={"/"}>
+                                                Volver a la lista de productos
+                                                </NavLink>
+                                            </div>
+                                            )
+                                        ) : (
+                                            <Loader />
+                                        )}
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            )}
+                    )}
         </>
     )
 };
--- lei-doiny-cakes/src/components/Cart/Formulario.js
+++ lei-doiny-cakes/src/components/Cart/Formulario.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React from 'react';
+import {Link} from
 import '../css/Main.css';
 
-const FormularioCompra = ({finalizarCompra, formData, setFormData}) => {
+const FormularioCompra = ({finalizarCompra, formularioData, setFormularioData}) => {
     return (
         <form>
             <div className="form-group">
                 <label for="formGroupExampleInput">Nombre y Apellido</label>
-                <input className="form-control" type="text" placeholder="Introducir el nombre" value={formData.name} onChange={(e) => setFormData({...formData, name: e.target.value})} required />
+                <input className="form-control" type="text" placeholder="Introducir el nombre" value={formularioData.name} onChange={(e) => setFormularioData({...formularioData, name: e.target.value})} required />
             </div>
             <div className="form-group">
                 <label for="formGroupExampleInput2">Telefono</label>
-                <input className="form-control" type="phone" placeholder="Ingrese Telefono" value={formData.phone} onChange={(e) => setFormData({ ...formData, phone: e.target.value })} required />
+                <input className="form-control" type="phone" placeholder="Ingrese Telefono" value={formularioData.phone} onChange={(e) => setFormularioData({ ...formularioData, phone: e.target.value })} required />
             </div>
             <div className="form-group">
                 <label for="formGroupExampleInput2">Email</label>
-                <input id="formGroupExampleInput2" className="form-control" type="text" name="Mail" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} required />
+                <input id="formGroupExampleInput2" className="form-control" type="text" name="Mail" value={formularioData.email} onChange={(e) => setFormularioData({ ...formularioData, email: e.target.value })} required />
             </div>
-            <div className="form-group">
-                <button  className="btn btn-secondary text-uppercase" type="submit" onClick={() => finalizarCompra()}> Finalizar compra</button>
+            <div className="row mx-4">
+                <div className="col d-flex justify-content-end">
+                    <Link id="botonCartFinal" className="btn btn-secondary" exact to={'/'}>Seguir comprando</Link>
+                    <button className="btn btn-secondary" type="button" onClick={() => finalizarCompra()}> Finalizar compra</button>
+                </div>
             </div>
         </form>
     );
